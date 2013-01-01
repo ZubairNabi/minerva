60e67a689e1424ae060fe245cc4067417e81dcc5
Zubair Nabi <zubair.nabi@cl.cam.ac.uk>
2013-01-01 13:45:05 +0500

    1) Security tests moved from mininet_setup to their own module, 2) No encryption test added.

--- src/minerva/tests/mininet_setup.py
+++ src/minerva/tests/mininet_setup.py
@@ -1,11 +1,5 @@
 from mininet.topo import Topo
-from mininet.net import Mininet
-from mininet.node import CPULimitedHost
-from mininet.link import TCLink
-from mininet.util import dumpNodeConnections, pmonitor
-from mininet.log import setLogLevel
-import time
-import sys
+
 
 class Link(object):
     def __init__(self, bandwidth, delay):
@@ -45,35 +39,4 @@ class ClientServerTopo(SingleSwitchTopo):
     "Single switch connected to a client."
     def __init__(self, link_obj):
         super(ClientServerTopo, self).__init__(1, link_obj, .5)
-        
-def securityTest(link_obj):
-    "Create a client server network and do a simple security test"
-    topo = ClientServerTopo(link_obj)
-    net = Mininet(topo=topo, 
-                  host=CPULimitedHost, link=TCLink)
-    net.start()
-    print "Dumping host connections"
-    dumpNodeConnections(net.hosts)
-    print "Conducting security test between client and server for a %s link" % link_obj
-    client, server = net.get('h1', 's1')
-    procs = {}
-    print "Executing server process on %s" % server.name
-    procs[server] = server.popen(['sudo', sys.executable, '/home/mininet/minerva/src/minerva/server/server.py', str(server.IP())])
-    #sleep to ensure that the server is running before we execute the client
-    time.sleep(2)
-    print "Executing client process on %s" % client.name
-    procs[client] = server.popen(['sudo', sys.executable, '/home/mininet/minerva/src/minerva/client/client.py', str(server.IP())])
-    for h, line in pmonitor(procs, timeoutms=500):
-        if h is None:
-            break
-    #the client is done. I keeel the server!
-    print "Cleaning up"
-    procs[server].kill()
-    net.stop()
-
-if __name__ == '__main__':
-    setLogLevel('info')
-    _3g_link = _3GLink()
-    wifi_link = WiFiLink()
-    securityTest(_3g_link)
-    securityTest(wifi_link)
+    
---
+++ src/minerva/tests/security_tests.py
@@ -0,0 +1,80 @@
+import time
+import sys
+from mininet.log import setLogLevel
+from mininet.util import dumpNodeConnections, pmonitor
+from mininet.node import CPULimitedHost
+from mininet.link import TCLink
+from mininet.net import Mininet
+
+
+from mininet_setup import ClientServerTopo, _3GLink, WiFiLink
+
+
+def encryptionTest(link_obj):
+    "Create a client server network and do a simple security test"
+    topo = ClientServerTopo(link_obj)
+    net = Mininet(topo=topo, 
+                  host=CPULimitedHost, link=TCLink)
+    print "Conducting encryptionTest between client and server for a %s link" % link_obj
+    net.start()
+    print "Dumping host connections"
+    dumpNodeConnections(net.hosts)
+    print "Conducting ze test"
+    client, server = net.get('h1', 's1')
+    procs = {}
+    print "Executing server process on %s" % server.name
+    procs[server] = server.popen(['sudo', sys.executable, '/home/mininet/minerva/src/minerva/server/server.py', str(server.IP())])
+    #sleep to ensure that the server is running before we execute the client
+    time.sleep(2)
+    print "Executing client process on %s" % client.name
+    procs[client] = server.popen(['sudo', sys.executable, '/home/mininet/minerva/src/minerva/client/client.py', str(server.IP())])
+    for h, line in pmonitor(procs, timeoutms=500):
+        if h is None:
+            break
+    #the client is done. I keeel the server!
+    print "Cleaning up"
+    procs[server].kill()
+    net.stop()
+    
+def noEncryptionTest(link_obj):
+    "Create a client server network and do a simple security test"
+    topo = ClientServerTopo(link_obj)
+    net = Mininet(topo=topo, 
+                  host=CPULimitedHost, link=TCLink)
+    print "Conducting encryptionTest between client and server for a %s link" % link_obj
+    net.start()
+    print "Dumping host connections"
+    dumpNodeConnections(net.hosts)
+    print "Conducting ze test"
+    client, server = net.get('h1', 's1')
+    procs = {}
+    print "Executing server process on %s" % server.name
+    procs[server] = server.popen(['sudo', sys.executable, 
+                                  '/home/mininet/minerva/src/minerva/server/server.py', 
+                                  str(server.IP()), 
+                                      'False'])
+    #sleep to ensure that the server is running before we execute the client
+    time.sleep(2)
+    print "Executing client process on %s" % client.name
+    procs[client] = server.popen(['sudo', sys.executable, 
+                                  '/home/mininet/minerva/src/minerva/client/client.py', 
+                                  str(server.IP()),
+                                      'False'])
+    for h, line in pmonitor(procs, timeoutms=500):
+        if h is None:
+            break
+    #the client is done. I keeel the server!
+    print "Cleaning up"
+    procs[server].kill()
+    net.stop()
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    _3g_link = _3GLink()
+    wifi_link = WiFiLink()
+    encryptionTest(_3g_link)
+    encryptionTest(wifi_link)
+    noEncryptionTest(_3g_link)
+    noEncryptionTest(wifi_link)
+    
+    
