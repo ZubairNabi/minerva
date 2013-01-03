6cbb4cc35084ca87bc9b4fc9a59f70f0120e7bde
Zubair Nabi <zubair.nabi@cl.cam.ac.uk>
2013-01-03 23:19:27 +0500

    Locks added for shared variables in handler.

--- src/minerva/server/handle_query.py
+++ src/minerva/server/handle_query.py
@@ -4,6 +4,8 @@ from fetch_query import Fetcher
 from parse_query import Parser
 from minerva.common.serialization import Serialization
 import minerva.common.RSACrypto as RSA
+from threading import Lock
+
 import minerva.common.AESCrypto as AES
 from ontology_manager import OntologyManager
 
@@ -14,8 +16,10 @@ class Handler(object):
         self.encryption = encryption
         # contains RegisterAgent objects indexed by client_id
         self.clients = {}
+        self.clients_lock = Lock()
         # contains registered user names for fast look up 
         self.registered_usernames = set()
+        self.registered_usernames_lock = Lock()
         self.client_id = 0
         self.rsa_key = RSA.generate_RSA_keypair()
         self.logger.info('Server RSA key mod %s, exp %s', str(self.rsa_key.n), str(self.rsa_key.e))
@@ -25,8 +29,10 @@ class Handler(object):
         
     def handle_query(self, message):
         query_message = Serialization.deserialize_sendquery(message)
+        self.clients_lock.acquire()
         if query_message.user_id in self.clients:
             aes_key = self.clients[query_message.user_id].symmetric_key
+            self.clients_lock.release()
             query = query_message.query
             if self.encryption:
                 query = AES._aes_decrypt_and_decode(aes_key, 
@@ -42,21 +48,28 @@ class Handler(object):
         return base64.b64encode(Serialization.serialize_getpublickeyresponse(self.rsa_key))
     
     def handle_register(self, register):
+        self.registered_usernames_lock.acquire()
         if register.username not in self.registered_usernames:
             self.registered_usernames.add(register.username)
             user_id = self.client_id
+            self.registered_usernames_lock.release()
             if self.encryption:
                 register.symmetric_key = RSA._rsa_decrypt_and_decode(self.rsa_key, 
                                                           register.symmetric_key)
+            self.clients_lock.acquire()
             self.clients[user_id] = register
+            self.client_id += 1  
+            self.clients_lock.release()
             self.__ontology_manager.add_individual('User', register.username)
             self.__ontology_manager.save()
-            self.client_id += 1  
             self.logger.debug('Username %s registered and assigned ID %d' \
                               % (register.username, user_id))
             if self.encryption:
-                return RSA.rsa_encrypt_client(self.clients[user_id], 
+                self.clients_lock.acquire()
+                ret = RSA.rsa_encrypt_client(self.clients[user_id], 
                                               Serialization.serialize_registeruserresponse(user_id))
+                self.clients_lock.release()
+                return ret
             return base64.b64encode(Serialization.serialize_registeruserresponse(user_id))
         self.logger.error('Username %s already registered' % register.username)
         return
