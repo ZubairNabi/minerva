6108baaf494befd25014bf7766378abd02d686ac
Zubair Nabi <zubair.nabi@cl.cam.ac.uk>
2013-01-03 10:16:55 +0500

    1) Redundant noEncryption method removed, 2) 2 second sleep added before the processes are whacked to ensure that they complete all their work.

--- src/minerva/tests/security_tests.py
+++ src/minerva/tests/security_tests.py
@@ -51,36 +51,8 @@ def encryptionTest(link_obj, encryption=True):
     for h, line in pmonitor(procs, timeoutms=500):
         if h is None:
             break
-    #the client is done. I keeel the server!
-    print "Cleaning up"
-    for p in procs.values():
-        p.send_signal(SIGKILL)
-    net.stop()
-    
-def noEncryptionTest(link_obj):
-    "Create a client server network and do a simple security test"
-    topo = ClientServerTopo(link_obj)
-    net = Mininet(topo=topo, 
-                  host=CPULimitedHost, link=TCLink)
-    print "Conducting encryptionTest between client and server for a %s link" % link_obj
-    net.start()
-    print "Dumping host connections"
-    dumpNodeConnections(net.hosts)
-    print "Conducting ze test"
-    client, server = net.get('h1', 's1')
-    procs = {}
-    print "Executing server process on %s" % server.name
-    
-    #sleep to ensure that the server is running before we execute the client
+    #wait for the processes to get their work done
     time.sleep(2)
-    print "Executing client process on %s" % client.name
-    procs[client] = server.popen([sys.executable, 
-                                  '/home/mininet/minerva/src/minerva/tests/client_tests.py', 
-                                  str(server.IP()),
-                                      'False'])
-    for h, line in pmonitor(procs, timeoutms=500):
-        if h is None:
-            break
     #the client is done. I keeel the server!
     print "Cleaning up"
     for p in procs.values():
